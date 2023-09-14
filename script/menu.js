401c5ad83c8f5945a3798bacc37ee66aad2ac0af
kamonlakp <108316764+kamonlakp@users.noreply.github.com>
2023-09-14 12:48:42 +0700

    update
    -add cart
    -try add food
    -แก้ css nav นิดหน่อย

--- script/menu.js
+++ script/menu.js
@@ -1,26 +1,8 @@
-const scrollingNav = document.getElementById('scrolling-nav');
-let isScrolling = false;
-let startX = 0;
-let scrollLeft = 0;
-
-scrollingNav.addEventListener('mousedown', (e) => {
-    isScrolling = true;
-    startX = e.pageX - scrollingNav.offsetLeft;
-    scrollLeft = scrollingNav.scrollLeft;
-});
-
-scrollingNav.addEventListener('mouseup', () => {
-    isScrolling = false;
-});
-
-scrollingNav.addEventListener('mouseleave', () => {
-    isScrolling = false;
-});
-
-scrollingNav.addEventListener('mousemove', (e) => {
-    if (!isScrolling) return;
-    e.preventDefault();
-    const x = e.pageX - scrollingNav.offsetLeft;
-    const walk = (x - startX);
-    scrollingNav.scrollLeft = scrollLeft - walk;
-});
+let nav = document.querySelector("nav");
+      window.addEventListener("scroll", () => {
+        if (document.documentElement.scrollTop > 20) {
+          nav.classList.add("category");
+        } else {
+          nav.classList.remove("category");
+        }
+      });
